ebd97c93ca9aba5aaddc167f106b1e50091aee62
Fernando <fmoleromartin@gmail.com>
2023-06-18 20:50:26 +0200

    Submit maquetacion

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -5,25 +5,36 @@ const SignInPage = (): JSX.Element => {
   return (
     <div className="sign-in-page page">
       <Header></Header>
+      <div className="sign-in-page__box">
+        <h2 className="sign-in-page__box-title">BIENVENIDO</h2>
+        <h3 className="sign-in-page__box-subtitle">SIGN IN</h3>
+        <p className="login-page__box-message">Introduce tus credenciales para acceder:</p>
+        <form className="sign-in-page__form">
+          <label className="sign-in-page__form-label" htmlFor="email">
+            Email:
+          </label>
+          <input className="login-page__form-input" type="text" id="email" placeholder="Introduce tu email" />
 
-      <form className="sign-in-page__form">
-        <h2 className="sign-in-page__form-title">Bienvenido</h2>
-        <h3 className="sign-in-page__form-subtitle">SIGN IN</h3>
-        <p>Introduce tus credenciales para acceder:</p>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" placeholder="Introduce tu email" />
+          <label className="sign-in-page__form-label" htmlFor="password">
+            Password
+          </label>
+          <input className="login-page__form-input" type="text" id="password" placeholder="Introduce tu contraseña" />
 
-        <label htmlFor="password">Password</label>
-        <input type="text" id="password" placeholder="Introduce tu contraseña" />
+          <label className="sign-in-page__form-label" htmlFor="name">
+            Nombre
+          </label>
+          <input className="login-page__form-input" type="text" id="name" placeholder="Introduce tu nombre" />
 
-        <label htmlFor="name">Nombre</label>
-        <input type="text" id="name" placeholder="Introduce tu nombre" />
+          <label className="sign-in-page__form-label" htmlFor="lastname">
+            Apellidos
+          </label>
+          <input className="login-page__form-input" type="text" id="lastname" placeholder="Introduce tus apellidos" />
 
-        <label htmlFor="lastname">Nombre</label>
-        <input type="text" id="lastname" placeholder="Introduce tu apellido" />
-
-        <input type="submit" title="Log in" />
-      </form>
+          <button className="sign-in-page__form-submit" type="submit">
+            REGISTRARSE
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
