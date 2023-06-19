0769387b731456ddaeb332160f274b13f850c50e
Fernando <fmoleromartin@gmail.com>
2023-06-20 00:35:14 +0200

    fix home error

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,14 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./HomePage.scss";
-import imageHome from "../../assets/home-image.png";
-import escudoAvatar from "../../assets/escudo-avatar.png";
+import teamLogo from "../../assets/team-logo.png";
+import homeImage from "../../assets/home-image.png";
 
 const HomePage = (): JSX.Element => {
   return (
     <div className="home-page page">
       <Header></Header>
-      <img className="home-page__image" src={imageHome} alt="Login avatar" />
+      <img className="home-page__image" src={homeImage} alt="header-home image" />
       <div className="home-page__containers">
         <div className="home-page__box">
           <h2 className="home-page__box-title">CLASIFICACIÓN</h2>
@@ -23,41 +23,86 @@ const HomePage = (): JSX.Element => {
               </tr>
             </thead>
             <tbody>
-              <tr className="home-page__table-spacer"></tr>
-              <tr className="home-page__table-spacer"></tr>
-              <tr className="home-page__table-row">
-                <td>1</td>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">1</td>
+                <td>
+                  <div className="home-page__table-team">
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
+                    <p>Equipo 1</p>
+                  </div>
+                </td>
+                <td>12</td>
+                <td>5</td>
+                <td>3</td>
+              </tr>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">2</td>
                 <td className="home-page__table-team">
-                  <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                  <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   <p>Equipo 1</p>
                 </td>
                 <td>12</td>
                 <td>5</td>
                 <td>3</td>
               </tr>
-              <tr className="home-page__table-spacer"></tr>
-              <tr className="home-page__table-row">
-                <td>1</td>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">3</td>
                 <td className="home-page__table-team">
-                  <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                  <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   <p>Equipo 1</p>
                 </td>
                 <td>12</td>
                 <td>5</td>
                 <td>3</td>
               </tr>
-              <tr className="home-page__table-spacer"></tr>
-              <tr className="home-page__table-row">
-                <td>1</td>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">4</td>
                 <td className="home-page__table-team">
-                  <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                  <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   <p>Equipo 1</p>
                 </td>
                 <td>12</td>
                 <td>5</td>
                 <td>3</td>
               </tr>
-              <tr className="home-page__table-spacer"></tr>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">5</td>
+                <td className="home-page__table-team">
+                  <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
+                  <p>Equipo 1</p>
+                </td>
+                <td>12</td>
+                <td>5</td>
+                <td>3</td>
+              </tr>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">6</td>
+                <td className="home-page__table-team">
+                  <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
+                  <p>Equipo 1</p>
+                </td>
+                <td>12</td>
+                <td>5</td>
+                <td>3</td>
+              </tr>
+              <tr className="home-page__spacer"></tr>
+              <tr className="home-page__table-clas">
+                <td className="home-page__table-number">7</td>
+                <td className="home-page__table-team">
+                  <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
+                  <p>Equipo 1</p>
+                </td>
+                <td>12</td>
+                <td>5</td>
+                <td>3</td>
+              </tr>
             </tbody>
           </table>
         </div>
@@ -74,13 +119,13 @@ const HomePage = (): JSX.Element => {
                 <div className="home-page__table-row">
                   <td>RMC</td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>
                     <div className="home-page__table-score">NO DISPUTADO</div>
                   </td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>BCN</td>
                 </div>
@@ -89,13 +134,13 @@ const HomePage = (): JSX.Element => {
                 <div className="home-page__table-row">
                   <td>RMC</td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>
                     <div className="home-page__table-score">10-0</div>
                   </td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>BCN</td>
                 </div>
@@ -104,13 +149,13 @@ const HomePage = (): JSX.Element => {
                 <div className="home-page__table-row">
                   <td>RMC</td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>
                     <div className="home-page__table-score">10-0</div>
                   </td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>BCN</td>
                 </div>
@@ -119,13 +164,13 @@ const HomePage = (): JSX.Element => {
                 <div className="home-page__table-row">
                   <td>RMC</td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>
                     <div className="home-page__table-score">10-0</div>
                   </td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>BCN</td>
                 </div>
@@ -134,30 +179,34 @@ const HomePage = (): JSX.Element => {
                 <div className="home-page__table-row">
                   <td>RMC</td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>
                     <div className="home-page__table-score">10-0</div>
                   </td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <img className="home-page__team-img" src={teamLogo} alt="Escudo avatar" />
                   </td>
                   <td>BCN</td>
                 </div>
               </tr>
               <tr>
                 <div className="home-page__table-row">
-                  <td>RMC</td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <span>RMC</span>
                   </td>
                   <td>
-                    <div className="home-page__table-score">10-0</div>
+                    <img className="home-page__team-img" src={teamLogo} alt="Team logo" />
                   </td>
                   <td>
-                    <img className="header__avatar" src={escudoAvatar} alt="Escudo avatar" />
+                    <div className="home-page__table-score">0-0</div>
                   </td>
-                  <td>BCN</td>
+                  <td>
+                    <img className="home-page__team-img" src={teamLogo} alt="Team logo" />
+                  </td>
+                  <td>
+                    <span>PLN</span>
+                  </td>
                 </div>
               </tr>
             </tbody>
