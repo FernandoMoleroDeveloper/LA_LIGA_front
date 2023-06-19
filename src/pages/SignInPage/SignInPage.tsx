664bd4dc0a1f529c687e33c0e515c8b75b140755
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-06-19 20:13:56 +0200

    Correccion footer en Signin

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -6,35 +6,37 @@ const SignInPage = (): JSX.Element => {
   return (
     <div className="sign-in-page page">
       <Header></Header>
-      <div className="sign-in-page__box">
-        <h2 className="sign-in-page__box-title">BIENVENIDO</h2>
-        <h3 className="sign-in-page__box-subtitle">SIGN IN</h3>
-        <p className="login-page__box-message">Introduce tus credenciales para acceder:</p>
-        <form className="sign-in-page__form">
-          <label className="sign-in-page__form-label" htmlFor="email">
-            Email:
-          </label>
-          <input className="login-page__form-input" type="text" id="email" placeholder="Introduce tu email" />
+      <div className="login-page__container">
+        <div className="sign-in-page__box">
+          <h2 className="sign-in-page__box-title">BIENVENIDO</h2>
+          <h3 className="sign-in-page__box-subtitle">SIGN IN</h3>
+          <p className="login-page__box-message">Introduce tus credenciales para acceder:</p>
+          <form className="sign-in-page__form">
+            <label className="sign-in-page__form-label" htmlFor="email">
+              Email:
+            </label>
+            <input className="login-page__form-input" type="text" id="email" placeholder="Introduce tu email" />
 
-          <label className="sign-in-page__form-label" htmlFor="password">
-            Password
-          </label>
-          <input className="login-page__form-input" type="text" id="password" placeholder="Introduce tu contraseña" />
+            <label className="sign-in-page__form-label" htmlFor="password">
+              Password
+            </label>
+            <input className="login-page__form-input" type="text" id="password" placeholder="Introduce tu contraseña" />
 
-          <label className="sign-in-page__form-label" htmlFor="name">
-            Nombre
-          </label>
-          <input className="login-page__form-input" type="text" id="name" placeholder="Introduce tu nombre" />
+            <label className="sign-in-page__form-label" htmlFor="name">
+              Nombre
+            </label>
+            <input className="login-page__form-input" type="text" id="name" placeholder="Introduce tu nombre" />
 
-          <label className="sign-in-page__form-label" htmlFor="lastname">
-            Apellidos
-          </label>
-          <input className="login-page__form-input" type="text" id="lastname" placeholder="Introduce tus apellidos" />
+            <label className="sign-in-page__form-label" htmlFor="lastname">
+              Apellidos
+            </label>
+            <input className="login-page__form-input" type="text" id="lastname" placeholder="Introduce tus apellidos" />
 
-          <button className="sign-in-page__form-submit" type="submit">
-            REGISTRARSE
-          </button>
-        </form>
+            <button className="sign-in-page__form-submit" type="submit">
+              REGISTRARSE
+            </button>
+          </form>
+        </div>
       </div>
       <Footer></Footer>
     </div>
