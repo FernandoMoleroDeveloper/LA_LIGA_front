cc65642a859382390c1b2b6ad4325cc156ff0ff7
Fernando <fmoleromartin@gmail.com>
2023-06-20 23:53:02 +0200

    Added logic of SignIn Page working

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,36 +1,56 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./SignInPage.scss";
-import React, { ChangeEvent, useState } from "react";
+import React, { useRef } from "react";
+
+const API_URL_SIGNIN = `${process.env.REACT_APP_API_URL as string}/user`;
+
+interface SignInInfo {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
 
 const SignInPage = (): JSX.Element => {
-  const API_URL_SIGNIN = `${process.env.REACT_APP_API_URL as string}/user`;
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [lastname, setLastname] = useState<string>("");
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
 
   const handleRegister = async (event: React.FormEvent): Promise<void> => {
     event.preventDefault();
 
-    try {
-      const response = fetch(API_URL_SIGNIN, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-          name,
-          lastname,
-        }),
-      });
-      const data = (await response).json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
+    const signinInfo: SignInInfo = {
+      email: emailRef?.current?.value as string,
+      password: passwordRef?.current?.value as string,
+      firstName: firstNameRef?.current?.value as string,
+      lastName: lastNameRef?.current?.value as string,
+    };
+
+    if (!signinInfo.email || !signinInfo.password || !signinInfo.firstName || !signinInfo.lastName) {
+      alert("Todos los campos son obligatorios!");
+    } else {
+      doSignInRequest(signinInfo);
     }
+  };
+
+  const doSignInRequest = (signinInfo: SignInInfo): void => {
+    fetch(API_URL_SIGNIN, {
+      method: "POST",
+      body: JSON.stringify(signinInfo),
+      headers: { "Content-Type": "application/json; charset=UTF-8" },
+    })
+      .then(async (response) => {
+        if (response.status !== 200) {
+          alert("Registro correcto");
+        }
+        return await response.json();
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Ha ocurrido un error en la petición");
+      });
   };
 
   return (
@@ -45,58 +65,22 @@ const SignInPage = (): JSX.Element => {
             <label className="sign-in-page__form-label" htmlFor="email">
               Email:
             </label>
-            <input
-              className="login-page__form-input"
-              type="text"
-              id="email"
-              placeholder="Introduce tu email"
-              value={email}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setEmail(e.target.value);
-              }}
-            />
+            <input className="login-page__form-input" ref={emailRef} type="text" id="email" placeholder="Introduce tu email" />
 
             <label className="sign-in-page__form-label" htmlFor="password">
               Password
             </label>
-            <input
-              className="login-page__form-input"
-              type="password"
-              id="password"
-              placeholder="Introduce tu contraseña"
-              value={password}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setPassword(e.target.value);
-              }}
-            />
+            <input className="login-page__form-input" type="password" id="password" placeholder="Introduce tu contraseña" ref={passwordRef} />
 
             <label className="sign-in-page__form-label" htmlFor="name">
               Nombre
             </label>
-            <input
-              className="login-page__form-input"
-              type="text"
-              id="name"
-              placeholder="Introduce tu nombre"
-              value={name}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setName(e.target.value);
-              }}
-            />
+            <input className="login-page__form-input" type="text" id="name" placeholder="Introduce tu nombre" ref={firstNameRef} />
 
             <label className="sign-in-page__form-label" htmlFor="lastname">
               Apellidos
             </label>
-            <input
-              className="login-page__form-input"
-              type="text"
-              id="lastname"
-              placeholder="Introduce tus apellidos"
-              value={lastname}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setLastname(e.target.value);
-              }}
-            />
+            <input className="login-page__form-input" type="text" id="lastname" placeholder="Introduce tus apellidos" ref={lastNameRef} />
 
             <button className="sign-in-page__form-submit" type="submit">
               REGISTRARSE
