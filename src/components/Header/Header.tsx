a643049afd0416958f2e6304e66c5d3456846dff
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-06-20 13:20:16 +0200

    Header funcionando cuando hace login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
-// import { useContext } from "react";
+import { useContext } from "react";
 import "./Header.scss";
 import { NavLink } from "react-router-dom";
 import loginAvatar from "../../assets/login-avatar.svg";
-// import { AuthContext } from "../../App";
+import { AuthContext } from "../../App";
 
 const Header = (): JSX.Element => {
-  // const authInfo = useContext(AuthContext);
+  const authInfo = useContext(AuthContext);
 
   return (
     <header className="header">
@@ -14,13 +14,21 @@ const Header = (): JSX.Element => {
       </NavLink>
 
       <div className="header__user-info">
-        <span className="header__dashboard">MI PERFIL</span>
-        <span className="header__logout">LOGOUT</span>
-
-        <NavLink to="/login" className="header__login">
-          LOGIN / REGISTRO
-          <img className="header__avatar" src={loginAvatar} alt="Login avatar" />
-        </NavLink>
+        {authInfo.userToken ? (
+          <>
+            <NavLink to="/dashboard">
+              <span className="header__dashboard">MI PERFIL</span>
+            </NavLink>
+            <NavLink className="header__logout" to="/">
+              <button className="header__dashboard" onClick={authInfo.logOut}>LOGOUT</button>
+            </NavLink>
+          </>
+        ) : (
+          <NavLink to="/login" className="header__login">
+            LOGIN / REGISTRO
+            <img className="header__avatar" src={loginAvatar} alt="Login avatar" />
+          </NavLink>
+        )}
       </div>
     </header>
   );
