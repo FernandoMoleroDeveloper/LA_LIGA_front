448dfae95149f24bb6117e888bed586a24e26d83
Daniel Ruiz <danielruimar@gmail.com>
2023-06-23 20:21:29 +0200

    Added color to active button

--- src/components/Dashboard/DashboardAdminButtons/DashboardAdminButtons.tsx
+++ src/components/Dashboard/DashboardAdminButtons/DashboardAdminButtons.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./DashboardAdminButtons.scss";
 
 interface DashboardAdminButtonsProps {
@@ -5,28 +6,32 @@ interface DashboardAdminButtonsProps {
 }
 
 const DashboardAdminButtons: React.FC<DashboardAdminButtonsProps> = ({ setActiveTable }): JSX.Element => {
+  const [activeButton, setActiveButton] = useState<"users" | "teams" | "calendar">("users");
   return (
     <div className="admin-btn">
       <button
-        className="admin-btn__areas"
+        className={`admin-btn__areas ${activeButton === "users" ? "active" : ""}`}
         onClick={() => {
           setActiveTable("users");
+          setActiveButton("users")
         }}
       >
         USUARIOS
       </button>
       <button
-        className="admin-btn__areas"
+        className={`admin-btn__areas ${activeButton === "teams" ? "active" : ""}`}
         onClick={() => {
           setActiveTable("teams");
+          setActiveButton("teams")
         }}
       >
         EQUIPOS
       </button>
       <button
-        className="admin-btn__areas"
+        className={`admin-btn__areas ${activeButton === "calendar" ? "active" : ""}`}
         onClick={() => {
           setActiveTable("calendar");
+          setActiveButton("calendar")
         }}
       >
         LIGA
