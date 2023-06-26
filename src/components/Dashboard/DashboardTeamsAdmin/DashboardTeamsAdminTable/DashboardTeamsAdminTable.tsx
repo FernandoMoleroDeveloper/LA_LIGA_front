3f30d826f9f89dfb9262187b5e855e1a4409b716
Fernando <fmoleromartin@gmail.com>
2023-06-27 00:50:41 +0200

    Falta alguna cosa

--- src/components/Dashboard/DashboardTeamsAdmin/DashboardTeamsAdminTable/DashboardTeamsAdminTable.tsx
+++ src/components/Dashboard/DashboardTeamsAdmin/DashboardTeamsAdminTable/DashboardTeamsAdminTable.tsx
@@ -1,22 +1,71 @@
-import { useState } from "react";
-import DashboardAdminCreateTeam from "../../DashboardAdminCreateTeam/DashboardAdminCreateTeam";
+import { useContext, useRef } from "react";
 import DashboardAdminPlayersTable from "../../DashboardAdminPlayers/DashboardAdminPlayersTable/DashboardAdminPlayersTable";
 import DashboardTeamsAdminRow from "../DashboardTeamsAdminRow/DashboardTeamsAdminRow";
 import "./DashboardTeamsAdminTable.scss";
+import { AuthContext } from "../../../../App";
+import { ROL } from "../../../../models/User";
 
 interface DashboardTeamsTableProps {
-  playersAdmin: any;
   teamsAdmin: any;
   setShowPlayerTeam: React.Dispatch<React.SetStateAction<boolean>>;
   showPlayerTeam: boolean;
 }
 
-const DashboardTeamsTable = ({ playersAdmin, teamsAdmin, setShowPlayerTeam, showPlayerTeam }: DashboardTeamsTableProps): JSX.Element => {
-  const [selectedTeamId, setSelectedTeamId] = useState("");
+const API_URL_NEW_TEAM = `${process.env.REACT_APP_API_URL as string}/team`;
+
+interface NewTeamInfo {
+  name: string;
+  initials: string;
+}
+
+const DashboardTeamsTable = ({ teamsAdmin, setShowPlayerTeam, showPlayerTeam }: DashboardTeamsTableProps): JSX.Element => {
+  const authInfo = useContext(AuthContext);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const initialsRef = useRef<HTMLInputElement>(null);
 
   const handleMostrarJugadores = (idEquipo: string): void => {
-    setSelectedTeamId(idEquipo);
     setShowPlayerTeam(true);
+  };
+
+  const submitForm = (event: React.FormEvent): void => {
+    //
+    event.preventDefault();
+
+    const newTeamInfo: NewTeamInfo = {
+      name: nameRef?.current?.value as string,
+      initials: initialsRef?.current?.value as string,
+    };
+
+    if (!newTeamInfo.name || !newTeamInfo.initials) {
+      alert("Nombre y initials son obligatorios!");
+    } else {
+      doLoginRequest(newTeamInfo);
+    }
+  };
+
+  const doLoginRequest = (newTeamInfo: NewTeamInfo): void => {
+    fetch(API_URL_NEW_TEAM, {
+      method: "POST",
+      body: JSON.stringify(newTeamInfo),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${authInfo?.userToken as string}`,
+      },
+    })
+      .then(async (response) => {
+        if (response.status !== 201) {
+          alert("Login incorrecto");
+        }
+        return await response.json();
+      })
+      .then((data) => {
+        alert("Equipo creado correctamente");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Ha ocurrido un error en la petición");
+      });
   };
   return (
     <>
@@ -46,10 +95,24 @@ const DashboardTeamsTable = ({ playersAdmin, teamsAdmin, setShowPlayerTeam, show
         </table>
       </div>
       <p>AÑADIR EQUIPO</p>
-      <div>
-        <DashboardAdminCreateTeam></DashboardAdminCreateTeam>
+      <div className="admin-create-team__team">
+        {authInfo?.userRol === ROL.ADMIN ? (
+          <form onSubmit={submitForm} className="admin-create-team____new-player">
+            <input ref={nameRef} className="admin-create-team__input" type="text" id="name" placeholder="Introduce nombre..." />
+            <input ref={initialsRef} className="admin-create-team__input" type="text" id="initials" placeholder="Introduce Alias..." />
+            <button className="admin-create-team__form-submit" type="submit" title="LogIn">
+              AÑADIR
+            </button>
+          </form>
+        ) : null}
       </div>
-      {showPlayerTeam && <DashboardAdminPlayersTable playersAdmin={playersAdmin} teamId={selectedTeamId}></DashboardAdminPlayersTable>}
+      {showPlayerTeam && (
+        <div>
+          {teamsAdmin.map((team: any) => {
+            return <DashboardAdminPlayersTable key={team?.team?._id} team={team?.team}></DashboardAdminPlayersTable>;
+          })}
+        </div>
+      )}
     </>
   );
 };
